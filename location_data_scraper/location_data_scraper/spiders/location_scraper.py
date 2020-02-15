2395aaab34666b0a518dae070c8dc2a8213c2332
Coran Mulcahy <coran.mulcahy@outlook.com>
2020-02-15 17:16:00 +1100

    making progress.. spider if funcitonal with no errors, but returning empty dictionary of store name and store address

--- location_data_scraper/location_data_scraper/spiders/location_scraper.py
+++ location_data_scraper/location_data_scraper/spiders/location_scraper.py
@@ -1,11 +1,14 @@
 import scrapy
 
 class StoreSpider(scrapy.Spider):
-    name = 'iga_scraper'
-    start_urls = [
-        'https://www.iga.com.au/stores/#view=storelocator'
-    ]
+    name = 'scraper'
+    start_urls = ['https://www.iga.com.au/stores/#view=storelocator']
 
     def parse(self, response):
-        title = response.css('title::text').extract()
-        yield {'titletext': title}
+
+        store_name = response.xpath("//span[@class='sf-storename']/text()").extract()
+        store_address = response.xpath("//p[@class='sf-storeaddress']/text()").extract()
+        yield {
+           'store_name' : store_name,
+            'store_address' : store_address
+        }
